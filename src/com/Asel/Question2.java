d7ad620dc7e2582039d167646b751afc176d27bd
Auraofdivinity <peirisasel373@gmail.com>
2019-07-08 09:34:28 +0530

    Question2 fix

--- src/com/Asel/Question2.java
+++ src/com/Asel/Question2.java
@@ -2,6 +2,21 @@ package com.Asel;
 
 public class Question2 {
     public static void main(String[] args) {
+        int [] array = {42, 3, 1 , 12 , 42};
 
+        checkFortyTwos(array);
+
+    }
+
+    public static void checkFortyTwos(int[] array){
+        int count = 0;
+
+        for(int i:array){
+            if(i == 42){
+                count++;
+            }
+        }
+
+        System.out.println(count);
     }
 }
