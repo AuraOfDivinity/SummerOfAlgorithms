bc64f40ce96ac8b33320bfd16a73c558dbd6bd59
Auraofdivinity <peirisasel373@gmail.com>
2019-07-10 10:20:43 +0530

    Changes to Q11 and Q12

--- src/Day2/Q11InverseArrays.java
+++ src/Day2/Q11InverseArrays.java
@@ -1,4 +1,33 @@
 package Day2;
 
+import java.util.Scanner;
+
 public class Q11InverseArrays {
+    public static void main(String[] args) {
+        Scanner scn =  new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int[] inputArr =  new int[n];
+        scn.nextLine();
+
+        for(int i = 0; i < n; i++){
+            inputArr[i] = scn.nextInt();
+        }
+
+        int [] inverseArr = getInverseArray(inputArr);
+        for(int i = 0; i < inverseArr.length; i++){
+            System.out.print(inverseArr[i] + ", ");
+        }
+    }
+
+
+    private static int[] getInverseArray(int[] original){
+       int[] inverseArr = new int[original.length];
+
+       for(int i = 0; i < original.length; i++){
+           inverseArr[original[i] - 1] = i + 1;
+       }
+
+       return inverseArr;
+    }
 }
--- src/Day2/Q12CheckMirronInverse.java
+++ src/Day2/Q12CheckMirronInverse.java
@@ -1,4 +1,41 @@
 package Day2;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Q12CheckMirronInverse {
+    public static void main(String[] args) {
+        Scanner scn =  new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int[] inputArr =  new int[n];
+        scn.nextLine();
+
+        for(int i = 0; i < n; i++){
+            inputArr[i] = scn.nextInt();
+        }
+
+        int [] inverseArr = getInverseArray(inputArr);
+
+        System.out.println(isMirrorInverse(inputArr, inverseArr));
+    }
+
+
+    private static int[] getInverseArray(int[] original){
+        int[] inverseArr = new int[original.length];
+
+        for(int i = 0; i < original.length; i++){
+            inverseArr[original[i] - 1] = i + 1;
+        }
+
+        return inverseArr;
+    }
+
+    private static boolean isMirrorInverse(int[] original, int[] inverse){
+        if (Arrays.equals(original, inverse)) {
+            return  true;
+        }
+        return false;
+    }
+
 }
